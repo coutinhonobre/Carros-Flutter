400f131d6c8cae8325357c9b12a404e1f99583b9
Igor Nobre <igor.nobre@maximatech.com.br>
2020-05-12 01:19:00 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/livroandroid/carros/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.livroandroid.carros
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,89 @@
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _text("Login", "Digite o login", controller: _tLogin),
+          SizedBox(
+            height: 10,
+          ),
+          _text("Senha", "Digite a senha", controller: _tSenha, password: true),
+          SizedBox(
+            height: 20,
+          ),
+          _button("Login", _onClickLogin)
+        ],
+      ),
+    );
+  }
+
+
+  _text(String label, String hint, {bool password = false, TextEditingController controller}) {
+    return TextFormField(
+      controller: controller,
+          obscureText: password,
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.blue,
+          ),
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(
+              fontSize: 25,
+              color: Colors.grey,
+            ),
+            hintText: hint,
+            hintStyle: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        );
+  }
+
+  _button(String text, Function onPressed, {Color cor = Colors.blue}) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: cor,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  void _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
+
+}
+
+
+
+
