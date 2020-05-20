794f0a944c1479ff3a6d9b0cf7fb9b5b5cc01f03
Igor Nobre <igor.nobre@maximatech.com.br>
2020-05-20 01:49:12 -0300

    Finalizado aba de carros

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -1,13 +1,20 @@
-
-
 import 'dart:convert' as convert;
 
 import 'package:carros/pages/carro/carro.dart';
 import 'package:http/http.dart' as http;
 
+class TipoCarro {
+  static final String classicos = "classicos";
+  static final String esportivos = "esportivos";
+  static final String luxo = "luxo";
+}
+
 class CarrosApi {
-  static Future<List<Carro>> getCarros() async {
-    var url = 'https://carros-springboot.herokuapp.com/api/v1/carros';
+  static Future<List<Carro>> getCarros(String tipo) async {
+    var url =
+        'https://carros-springboot.herokuapp.com/api/v1/carros/tipo/$tipo';
+
+    print("GET > $url");
 
     var response = await http.get(url);
 
@@ -16,6 +23,5 @@ class CarrosApi {
     List list = convert.json.decode(json);
 
     return list.map<Carro>((map) => Carro.fromJson(map)).toList();
-
   }
 }
--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,103 +1,67 @@
 import 'package:carros/drawer_list.dart';
 import 'package:carros/pages/carro/carro.dart';
 import 'package:carros/pages/carro/carros_api.dart';
+import 'package:carros/pages/carro/carros_listview.dart';
 import 'package:carros/utils/alert.dart';
+import 'package:carros/utils/prefs.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+
+    Future<int> future = Prefs.getInt('tabIdx');
+
+    future.then((int tabIdx){
+      _tabController.index = tabIdx;
+    });
+
+    _tabController.addListener(() {
+      Prefs.setInt("tabIdx", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Carros"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: "Classicos",
+            ),
+            Tab(
+              text: "Esportivos",
+            ),
+            Tab(
+              text: "Luxo",
+            ),
+          ],
+        ),
       ),
-      body: _body(),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CarrosListView(TipoCarro.classicos),
+          CarrosListView(TipoCarro.esportivos),
+          CarrosListView(TipoCarro.luxo)
+        ],
+      ),
       drawer: DrawerList(),
     );
   }
-}
-
-_body() {
-  Future<List<Carro>> future = CarrosApi.getCarros();
-
-  return FutureBuilder(
-    future: future,
-    builder: (context, snapshot) {
-      if (snapshot.hasError) {
-        return Center(
-          child: Text(
-            "Não foi possível buscar os carros ${snapshot.error.toString()}.",
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 16,
-            ),
-          ),
-        );
-      }
-
-      if (!snapshot.hasData) {
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-
-      List<Carro> carros = snapshot.data;
-      return _listView(carros);
-    },
-  );
-}
-
-Container _listView(List<Carro> carros) {
-  return Container(
-    padding: EdgeInsets.all(16),
-    child: ListView.builder(
-        itemCount: carros != null ? carros.length : 0,
-        itemBuilder: (context, index) {
-          Carro c = carros[index];
-
-          return Card(
-            color: Colors.grey[200],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: Image.network(
-                      c.urlFoto ?? "http://www.livroandroid.com.br/livro/carros/classicos/Chevrolet_BelAir.png",
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    c.nome ?? "",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "Descrição...",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('DETALHES'),
-                        onPressed: () {
-                          /* ... */
-                        },
-                      ),
-                      FlatButton(
-                        child: const Text('SHARE'),
-                        onPressed: () {
-                          /* ... */
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        }),
-  );
 }
